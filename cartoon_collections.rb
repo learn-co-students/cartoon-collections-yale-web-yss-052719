909976e01891e901e91bb819a9596c14a5d2bdf5
Elijah Florence <efloren@siue.edu>
2019-05-16 16:18:50 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,21 @@
 def roll_call_dwarves(array)
-  do array.each |dwarf|
-    puts(dwarf)
+  do array.each_with_index |dwarf, pos|
+    pos ++
+    puts(pos + index)
 end
 
 def summon_captain_planet(array)
  do array.collect |power|
-  
+ return(power.capitalize + "!")
 end
 
-def long_planeteer_calls# code an argument here
- 
+def long_planeteer_calls(array)
+ array.any? do |calls|
+   call.length > 4
 end
 
-def find_the_cheese# code an argument here
- 
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find |cheese|
+  cheese_types.include?(cheese)
 end
